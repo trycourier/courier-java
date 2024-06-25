5e33eeec92d5b91a58a929dc6b2d681161a54773
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-25 20:53:03 +0000

    Release v3.1.1

--- src/main/java/com/courier/api/core/ClientOptions.java
+++ src/main/java/com/courier/api/core/ClientOptions.java
@@ -30,7 +30,7 @@ public final class ClientOptions {
             {
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.courier.fern:api-sdk");
-                put("X-Fern-SDK-Version", "v3.1.0");
+                put("X-Fern-SDK-Version", "v3.1.1");
             }
         });
         this.headerSuppliers = headerSuppliers;
